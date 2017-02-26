fbfd1854d18ade51e6be962047cfb6c28e3f590d
BraxHarrison <sanscomprehension@gmail.com>
2017-02-26 04:39:45 -0500

    FP-Breetlison

--- src/GUI/OverworldController.java
+++ src/GUI/OverworldController.java
@@ -1,18 +1,17 @@
 package GUI;
 
-import com.sun.javafx.collections.ObservableSequentialListWrapper;
 import edu.bsu.cs222.FPBreetlison.Character;
 import edu.bsu.cs222.FPBreetlison.GameController;
 import edu.bsu.cs222.FPBreetlison.GameData;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class OverworldController {
@@ -35,6 +34,8 @@ public class OverworldController {
     private int selectedAction;
     private int selectedItem;
     private int selectedAttack;
+    private boolean canAdvanceText;
+    private ArrayList<String> textForConsole;
 
     GameData gameData = GameData.getData();
     GameController game = new GameController();
@@ -44,7 +45,9 @@ public class OverworldController {
 
     @FXML
     public void initialize(){
+        textForConsole = new ArrayList<String>();
         setCharacterButtons();
+        pushMessage("Two enemy Jags appear!");
     }
     private void setCharacterButtons() {
         ArrayList<Character> team = gameData.getTeam();
@@ -63,14 +66,10 @@ public class OverworldController {
         }
     }
 
-    public void characterSelect(ActionEvent actionEvent) {
-        Button selectedButton = (Button) actionEvent.getSource();
-        String selectedCharacter = selectedButton.getId();
-        showCharacterMenu(selectedCharacter);
-
-    }
-
-    private void showCharacterMenu(String selectedCharacter) {
+    private void showCharacterMenu() {
+        actionMenu.setVisible(true);
+        characterMenu.setVisible(false);
+        pushMessage("What will " + gameData.getTeam().get(selectedCharacter).getName() + " do?" );
 
     }
 
@@ -92,22 +91,24 @@ public class OverworldController {
     }
     private void generateButtons(){
 
-
-
     }
 
-
+    //region Button Logic
     public void selectCharacter1(ActionEvent actionEvent) {
         selectedCharacter=0;
+        showCharacterMenu();
     }
     public void selectCharacter2(ActionEvent actionEvent) {
         selectedCharacter=1;
+        showCharacterMenu();
     }
     public void selectCharacter3(ActionEvent actionEvent) {
         selectedCharacter=2;
+        showCharacterMenu();
     }
     public void selectCharacter4(ActionEvent actionEvent) {
         selectedCharacter=3;
+        showCharacterMenu();
     }
 
     public void selectAttack(ActionEvent actionEvent) {
@@ -115,11 +116,37 @@ public class OverworldController {
     }
 
     public void selectSkill(ActionEvent actionEvent) {
+
     }
 
     public void selectItems(ActionEvent actionEvent) {
+
     }
 
     public void selectFlee(ActionEvent actionEvent) {
+
     }
+
+    public void pushMessage(String message){
+        textForConsole.add(message);
+        advanceText();
+    }
+    public void advanceText() {
+        String text = "";
+        for(int i = 0; i<textForConsole.size();i++){
+            text += "\n" + textForConsole.get(i) + "\n";
+        }
+        mainDisplay.setText(mainDisplay.getText() + text);
+        textForConsole.clear();
+
+    }
+
+    public void backToCharacterSelect(ActionEvent actionEvent) {
+        actionMenu.setVisible(false);
+        characterMenu.setVisible(true);
+    }
+
+    //endregion
+
+
 }
