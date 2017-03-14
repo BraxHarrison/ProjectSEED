f4be06c73288f1c87c5c4429047c57268e9e9cce
tetri <mbratton@bsu.edu>
2017-03-14 08:44:56 -0400

    - Final Version for 1.0.0.  Removed Extra Comments

--- src/edu/bsu/cs222/FPBreetlison/ElementType.java
+++ src/edu/bsu/cs222/FPBreetlison/ElementType.java
@@ -2,17 +2,12 @@ package edu.bsu.cs222.FPBreetlison;
 
 public class ElementType {
 
-    //We need a way to construct it with information pointing to weaknesses
-    //and strengths
-
-
     public ElementType(String name){
 
     }
 
     public String[] weaknesses(){
-        //This could return an array of strings of the elements this element is \
-        //weak against
+       \
         String[] weaknesses = {"Pyro", "Metal"};
         return weaknesses;
     }
